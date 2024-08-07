1b31a3996b62fc47fd075f2dacd2ca2ab8d5b3ca
Antônio Carlos Stangari Junior - Sonda <antonio.junior.sonda@elo.com.br>
2024-08-07 19:33:49 -0300

    commit entity

---
+++ src/main/java/com/projects/cadastropf/entity/Cadastro.java
@@ -0,0 +1,55 @@
+package com.projects.cadastropf.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "cadastros")
+public class Cadastro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private int idade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
